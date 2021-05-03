a0e09c402f834f5b422687fac60668cf2601866b
swapnil <swapnil@dunzo.in>
2021-05-03 23:48:03 +0530

    Add favourite

--- building_layout/lib/main.dart
+++ building_layout/lib/main.dart
@@ -30,11 +30,7 @@ class MyApp extends StatelessWidget {
               )
             ],
           )),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          FavouriteWidget()
         ],
       ),
     );
@@ -100,5 +96,50 @@ class MyApp extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class FavouriteWidget extends StatefulWidget {
+  @override
+  _FavouriteWidgetState createState() => _FavouriteWidgetState();
+}
+
+class _FavouriteWidgetState extends State<FavouriteWidget> {
+  bool _isFavourited = true;
+  int _favouriteCount = 41;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+            padding: EdgeInsets.all(0),
+            child: IconButton(
+              alignment: Alignment.centerRight,
+              icon:
+                  (_isFavourited ? Icon(Icons.star) : Icon(Icons.star_border)),
+              color: Colors.red[500],
+              onPressed: _toggleFavourite,
+            )),
+        SizedBox(
+          width: 18,
+          child: Text(
+            '$_favouriteCount',
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavourite() {
+    setState(() {
+      if (_isFavourited) {
+        _favouriteCount -= 1;
+        _isFavourited = false;
+      } else {
+        _favouriteCount += 1;
+        _isFavourited = true;
+      }
+    });
   }
 }
